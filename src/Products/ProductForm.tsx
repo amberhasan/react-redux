1efeaef27b1c414d41f731b113d87c37f6100e4e
amberhasan <amber.hasan@gmail.com>
2023-02-09 16:16:15 -0600

    Redux for values

--- src/Products/ProductForm.tsx
+++ src/Products/ProductForm.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Product } from "./products.slice";
 
-interface ProductFormProps {}
+const ProductForm: React.FC = () => {
+  const [{ title, price, id }, setProduct] = useState<Product>({
+    id: "",
+    title: "",
+    price: 0,
+  });
 
-const ProductForm: React.FC<ProductFormProps> = ({}) => {
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setProduct((prev) => {
+      (prev as any)[name] = value;
+      const newValue = { ...prev };
+      return newValue; //if you change this to return prev, then as you change the values, nothing would show as changed bc React would return prev, not the new stuff
+    });
   return (
     <>
       <h2>Add Game To The Store</h2>
       <form>
-        <input type="text" placeholder="Game title" name="title" />
-        <input type="number" placeholder="Price" name="price" />
-        <input type="text" placeholder="Id" name="id" />
+        <input
+          type="text"
+          placeholder="Game title"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          name="price"
+          value={price}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Id"
+          name="id"
+          value={id}
+          onChange={handleChange}
+        />
         <button>Add price</button>
       </form>
     </>
