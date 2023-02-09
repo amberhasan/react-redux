1cd38e077a913ee030778d4030fe01c0a5902cf3
amberhasan <amber.hasan@gmail.com>
2023-02-09 14:48:56 -0600

    Convert to Redux

--- src/Products/ProductsList.tsx
+++ src/Products/ProductsList.tsx
@@ -1,45 +1,20 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { getProductsSelector } from "./products.slice";
 
 interface ProductsListProps {}
 
-const initialProducts = [
-  { title: "Escape From Tarkov", price: 60, id: "eft" },
-  { title: "Hunt: Showdown", price: 70, id: "hunt" },
-  { title: "The Last of Us", price: 55, id: "hil" },
-];
-
-interface Product {
-  title: string;
-  price: number;
-  id: string;
-}
-
 const ProductsList: React.FC<ProductsListProps> = ({}) => {
-  const [products, setProducts] = useState<Product[]>(initialProducts);
+  const products = useSelector(getProductsSelector);
 
   return (
     <div>
       <h2>Games List</h2>
-      {initialProducts.map((product) => (
+      {products.map((product) => (
         <div key={product.id}>
           <span>{`${product.title} : ${product.price}`}</span>
         </div>
       ))}
-
-      <button
-        onClick={() =>
-          setProducts((prevProducts) => [
-            {
-              title: "Half Life",
-              price: 100,
-              id: "h1",
-            },
-            ...prevProducts,
-          ])
-        }
-      >
-        Add Product
-      </button>
     </div>
   );
 };
